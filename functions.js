f81e8ac983c4a3af62ab19da0410507ebcbe006d
Jay <179924362+CedricSmith00@users.noreply.github.com>
2024-09-06 14:10:52 +0100

    added activity 1-3

---
+++ functions.js
@@ -0,0 +1,24 @@
+//Activity 1
+let factorial = (n) => {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+console.log(factorial(33));
+
+// Activity 2
+let orderCount = 0;
+
+let drink = "Fanta";
+
+const takeOrder = (topping, drink) => {
+    console.log(`Order #${orderCount}: Pizza with ${topping} and a ${drink} to wash it all down with, please.`);
+    orderCount++;
+}
+
+takeOrder("Pineapple", "Fanta");
+takeOrder("Pepperoni", "Coke");
+takeOrder("Ham", "Sprite");
